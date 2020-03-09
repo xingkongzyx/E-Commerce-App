566b01a197cb1278dddadb0df659806b28e7b470
Yuxuan Zhu <zyx97agm@gmail.com>
2020-03-09 16:31:18 +0800

    image upload and save to json

--- routers/admin/products.js
+++ routers/admin/products.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const productsRepo = require("../../repositories/products");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
+const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 // Create a new router
 const router = express.Router();
-
+const upload = multer({ storage: multer.memoryStorage() });
+// const upload = multer({ dest: "uploads" });
 // List all products to user
 router.get("/admin/products", (req, res) => {
 	res.send("");
@@ -16,12 +19,24 @@ router.get("/admin/products/new", (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-	const errors = validationResult(req);
-	req.on("data", data => {
-		console.log(data.toString());
-	});
-	res.send("Submitted");
-});
+router.post(
+	"/admin/products/new",
+	// 其中name是我们表格中img的name
+	upload.single("image"),
+	[requireTitle, requirePrice],
+	async (req, res) => {
+		const errors = validationResult(req);
+		// If some errors during validation
+		if (!errors.isEmpty()) {
+			return res.send(productsNewTemplate({ errors }));
+		}
+		// 以string的形式存储image
+		const image = req.file.buffer.toString("base64");
+		const { title, price } = req.body;
+		// create a new product
+		await productsRepo.create({ title, image, price });
+		res.send("Submitted");
+	}
+);
 
 module.exports = router;
